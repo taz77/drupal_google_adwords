dd597c6add637dd0a09cb4efb8309ef228090d12
Jason Savino <jasonrsavino@gmail.com>
2013-12-11 13:40:52 -0800

    making google_adwords a field for more flexibility

---
+++ google_adwords.tpl.php
@@ -0,0 +1,14 @@
+<!-- Google Code for Conversion Page //-->
+<script language="JavaScript" type="text/javascript">
+<!--
+    var google_conversion_id = <?php print $id; ?>;
+    var google_conversion_language = "<?php print $language; ?>";
+    var google_conversion_format = "<?php print $format; ?>";
+    var google_conversion_color = "<?php print $color; ?>";
+    var google_conversion_label = "<?php print $label; ?>";
+//-->
+</script>
+<script language="JavaScript" src="<?php print $google_js; ?>"></script>
+<noscript>
+    <img height="1" width="1" border="0" src="https://www.googleadservices.com/pagead/conversion/<?php print $id; ?>/?label=<?php print $label; ?>&amp;guid=ON&amp;script=0"/>
+</noscript>
