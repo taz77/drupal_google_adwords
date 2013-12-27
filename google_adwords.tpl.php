e4abf996906eaa6d8d30a968a93342e945fbd25d
mglaman <mglaman@2416470.no-reply.drupal.org>
2013-12-27 13:51:16 +0100

    Issue #2162213 Correcting the template variables

--- google_adwords.tpl.php
+++ google_adwords.tpl.php
@@ -1,14 +1,14 @@
 <!-- Google Code for Conversion Page //-->
 <script language="JavaScript" type="text/javascript">
-/* <![CDATA[ */
-    var google_conversion_id = <?php print $id; ?>;
-    var google_conversion_language = "<?php print $language; ?>";
-    var google_conversion_format = "<?php print $format; ?>";
-    var google_conversion_color = "<?php print $color; ?>";
-    var google_conversion_label = "<?php print $label; ?>";
-/* ]]> */
+  /* <![CDATA[ */
+  var google_conversion_id = <?php print $conversion_id; ?>;
+  var google_conversion_language = "<?php print $conversion_language; ?>";
+  var google_conversion_format = "<?php print $conversion_format; ?>";
+  var google_conversion_color = "<?php print $conversion_color; ?>";
+  var google_conversion_label = "<?php print $conversion_label; ?>";
+  /* ]]> */
 </script>
 <script language="JavaScript" src="<?php print $google_js; ?>"></script>
 <noscript>
-    <img height="1" width="1" border="0" src="https://www.googleadservices.com/pagead/conversion/<?php print $id; ?>/?label=<?php print $label; ?>&amp;guid=ON&amp;script=0"/>
+<img height="1" width="1" border="0" src="https://www.googleadservices.com/pagead/conversion/<?php print $conversion_id; ?>/?label=<?php print $conversion_label; ?>&amp;guid=ON&amp;script=0"/>
 </noscript>
